bc192b211c0b7b5a185745affedab9911a72f133
sk <sk@axa.com>
2023-04-27 00:20:13 +0530

    3rd commit

--- build.sh
+++ build.sh
@@ -5,7 +5,8 @@ echo "Get the Image name from Parameterized variable:"
 echo "Get the Image version from Parameterized variable:"
 
 # Build Docker image from Dockerfile
-docker build -t $IMAGE_NAME:$IMAGE_VERSION .
+docker build -t "$IMAGE_NAME:$IMAGE_VERSION" .
+echo "Docker image build completed successfully."
 
 # Display list of Docker images
 docker images
--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 # Push Docker image to Docker Hub based on the branch name and the auto source code trigger
 if [[ "$GIT_BRANCH" == "dev" ]]; then
     echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-    docker tag $IMAGE_NAME:$IMAGE_VERSION "$DOCKER_REPO:$DOCKER_TAG"
+    docker tag "$IMAGE_NAME:$IMAGE_VERSION" "$DOCKER_REPO:$DOCKER_TAG"
     docker push "$DOCKER_REPO:$DOCKER_TAG"
     docker logout
 elif [[ "$GIT_BRANCH" == "master" ]]; then
     echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
-    docker tag $IMAGE_NAME:$IMAGE_VERSION "$DOCKER_REPO:$DOCKER_TAG"
+    docker tag "$IMAGE_NAME:$IMAGE_VERSION" "$DOCKER_REPO:$DOCKER_TAG"
     docker push "$DOCKER_REPO:$DOCKER_TAG"
     docker logout
 fi
